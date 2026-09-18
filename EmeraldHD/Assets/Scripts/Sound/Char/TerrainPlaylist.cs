a916b8507de8cbfe7679ef4fb326d0de3c31e358
lodos3 <djgozzett@gmail.com>
2026-09-18 22:30:48 +0100

    Trainer Nexus clean JitPack APK build

--- EmeraldHD/Assets/Scripts/Sound/Char/TerrainPlaylist.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TerrainPlaylist : MonoBehaviour
-{
-    [SerializeField] public SoundCueGraph[] TextureSounds;
-}
